e1c8a5729e4bb14c7f85240bad716a84bab0ef97
Michael Lindon <michael.s.lindon@gmail.com>
2017-02-11 15:55:07 -0500

    added ssm dep

--- project.clj
+++ project.clj
@@ -3,10 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
-            [refactor-nrepl "2.3.0-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [incanter "1.9.1"]
+                 [ssm4clj "0.1.0-SNAPSHOT"]
                  [org.clojure/data.avl "0.0.17"]
                  [net.mikera/core.matrix "0.57.0"]
                  [net.mikera/vectorz-clj "0.45.0"]
